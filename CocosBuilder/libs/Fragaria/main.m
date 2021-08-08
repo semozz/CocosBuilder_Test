f4782a6c24ccfbbdc18efbae1daf2973d4cdc509
semo <>
2021-08-08 18:51:42 +0900

    first Time

---
+++ CocosBuilder/libs/Fragaria/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fragaria
+//
+//  Created by Jonathan on 30/04/2010.
+//  Copyright 2010 mugginsoft.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    glewInit();
+    
+    return NSApplicationMain(argc,  (const char **) argv);
+}
